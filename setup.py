2ab62014e8a2fefee3eb27d4ff2d14c3284029e2
Régis Eduardo Crestani <regis.crestani@gmail.com>
2016-02-13 00:18:36 -0200

    some adjustments in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
 from codecs import open
 from os import path
+
+from django_environments import __version__
 
 here = path.abspath(path.dirname(__file__))
 
@@ -16,27 +10,25 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-from django_environments import __version__
-
 setup(
-    name='django-environments',
-    version=__version__,
-    description='Smart environment controller for Django',
-    long_description=long_description,
-    url='http://www.django-environments.org/',
-    author='Régis Eduardo Crestani',
-    author_email='regis.crestani@gmail.com',
-    license='BSD',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Framework :: Django',
-        'Topic :: Software Development :: Utilities',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ],
-    packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=[],
+        name='django-environments',
+        version=__version__,
+        description='Smart environment controller for Django',
+        long_description=long_description,
+        url='http://www.django-environments.org/',
+        author='Régis Eduardo Crestani',
+        author_email='regis.crestani@gmail.com',
+        license='BSD',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Framework :: Django',
+            'Topic :: Software Development :: Utilities',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+        ],
+        packages=find_packages(exclude=['docs', 'tests']),
+        install_requires=[],
 )
